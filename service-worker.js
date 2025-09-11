10af667073838bf7485b3b15406b689c5e5aafff
Dương Tuấn Phong <114390987+Tuanphong3108@users.noreply.github.com>
2025-09-11 21:41:51 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// service-worker.js
+const CACHE_NAME = "gallery-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "viewer.js",
+  "manifest.json",
+  "gallery.png" // icon
+];
+
+// Cài service worker + cache file
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Load offline từ cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache khi có version mới
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
